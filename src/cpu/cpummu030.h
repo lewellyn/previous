d56c10b7e3276a033d221fbd874d561f8e32bd66
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2012-09-09 08:37:47 +0000

    Hopefully last patch for table search function.
    git-svn-id: http://svn.code.sf.net/p/previous/code/branches/branch_mmu@210 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/cpu/cpummu030.h
+++ src/cpu/cpummu030.h
@@ -7,7 +7,7 @@ void mmu_op30_pflush (uaecptr pc, uae_u32 opcode, uae_u16 next, uaecptr extra);
 uae_u32 mmu_op30_helper_get_fc(uae_u16 next);
 
 void mmu030_ptest_atc_search(uaecptr logical_addr, uae_u32 function_code, bool write);
-uae_u32 mmu030_ptest_table_search(uaecptr addr, bool super, bool data, bool write, int level);
+uae_u32 mmu030_table_search(uaecptr addr, bool super, bool data, bool write, int level);
 
 
 typedef struct {
@@ -21,7 +21,6 @@ void mmu030_decode_rp(uae_u64 RP);
 
 uae_u8 mmu030_get_fc(bool super, bool data);
 
-void mmu030_create_atc_entry(uaecptr addr, bool super, bool data, bool write);
 int mmu030_logical_is_in_atc(uaecptr addr, bool super, bool data, bool write);
 void mmu030_atc_handle_history_bit(int entry_num);
 
