07af98241e045457f29591f9905b879e11d91af2
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2014-03-16 18:39:28 +0000

    Minor optimization for mouse GUI.
    git-svn-id: http://svn.code.sf.net/p/previous/code/branches/branch_mmu@340 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/gui-sdl/dlgMouse.c
+++ src/gui-sdl/dlgMouse.c
@@ -24,19 +24,19 @@ char accel_locked[8];
 static SGOBJ mousedlg[] =
 {
 	{ SGBOX, 0, 0, 0,0, 46,22, NULL },
-    { SGTEXT, 0, 0, 16,1, 9,1, "Mouse options" },
+    { SGTEXT, 0, 0, 16,1, 13,1, "Mouse options" },
 
 	{ SGBOX, 0, 0, 1,4, 43,5, NULL },
-	{ SGTEXT, 0, 0, 2,5, 30,1, "Mouse acceleration in normal mode:" },
+	{ SGTEXT, 0, 0, 2,5, 34,1, "Mouse acceleration in normal mode:" },
     { SGTEXT, 0, 0, 2,7, 35,1, "Enter a value between 0.1 and 10.0:" },
 	{ SGEDITFIELD, 0, 0, 38,7, 4,1, accel_normal },
     
     { SGBOX, 0, 0, 1,10, 43,5, NULL },
-    { SGTEXT, 0, 0, 2,11, 30,1, "Mouse acceleration in locked mode:" },
+    { SGTEXT, 0, 0, 2,11, 34,1, "Mouse acceleration in locked mode:" },
     { SGTEXT, 0, 0, 2,13, 35,1, "Enter a value between 0.1 and 10.0:" },
 	{ SGEDITFIELD, 0, 0, 38,13, 4,1, accel_locked },
     
-	{ SGCHECKBOX, 0, 0, 2,16, 20,1, "Enable auto-grabbing" },
+	{ SGCHECKBOX, 0, 0, 2,16, 21,1, "Enable auto-locking" },
     
 	{ SGBUTTON, SG_DEFAULT, 0, 14,19, 20,1, "Back to main menu" },
 	{ -1, 0, 0, 0,0, 0,0, NULL }
@@ -45,38 +45,32 @@ static SGOBJ mousedlg[] =
 float read_float_string(char *s)
 {
     int i;
-    float result;
-    float left=0.0;
-    float right=0.0;
-    float factor=0.0;
+    float result=0.0;
     
     for (i=0; i<8; i++) {
         if (*s>=(0+'0') && *s<=(9+'0')) {
-            left *= 10.0;
-            left += (float)(*s-'0');
+            result *= 10.0;
+            result += (float)(*s-'0');
             s++;
         } else {
             if (i==0 && *s!='.' && *s!=',') /* bad input, default to 1.0 */
-                right=1.0;
+                result=1.0;
             break;
         }
     }
 
     if (*s == '.' || *s == ',') {
         s++;
-        factor=10.0;
-        for (i=0; i<8; i++) {
-            if (*s>=(0+'0') && *s<=(9+'0')) {
-                right += (float)(*s-'0')/factor;
-                factor*=10;
-                s++;
-            } else {
-                break;
+        if (*s>=(0+'0') && *s<=(9+'0')) {
+            result += (float)(*s-'0')/10.0;
+            s++;
+        }
+        if (*s>=(0+'0') && *s<=(9+'0')) {
+            if ((*s-'0')>=5) {
+                result+=0.1;
             }
         }
     }
-    
-    result=left+right;
     
     if (result<0.1)
         result=0.1;
